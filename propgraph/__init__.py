aa04dcd9746c9a844111bf153b6a22027a8f1449
Alex Milowski <alex@microbyre.com>
2022-07-06 14:54:38 -0700

    updated version

--- propgraph/__init__.py
+++ propgraph/__init__.py
@@ -4,4 +4,4 @@ from .schema import SchemaParser, Schema, NodeDefinition, EdgeDefinition
 __all__ = ['read_graph', 'graph_to_cypher', 'cypher_literal', 'cypher_for_item', 'cypher_for_node', 'cypher_for_edge_relation', 'NodeItem', 'EdgeRelationItem'
            'SchemaParser','Schema','NodeDefinition','EdgeDefinition']
 
-__version__ = '0.3'
+__version__ = '0.4'
