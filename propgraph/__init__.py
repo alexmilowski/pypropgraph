dc936e258cddf81d3206926c21898137988db4e8
Alex Milowski <alex@microbyre.com>
2022-10-25 15:28:36 -0700

    switched to new package metadata

--- propgraph/__init__.py
+++ propgraph/__init__.py
@@ -1,7 +1,10 @@
+"""A property graphs library for importing and generating cypher in python"""
+__version__=(0,5,0)
+__author__='Alex Miłowski'
+__author_email__='alex@milowski.com'
+
 from .cypher import read_graph, graph_to_cypher, cypher_literal, cypher_for_item, cypher_for_node, cypher_for_edge_relation, NodeItem, EdgeRelationItem
 from .schema import SchemaParser, Schema, NodeDefinition, EdgeDefinition
 
 __all__ = ['read_graph', 'graph_to_cypher', 'cypher_literal', 'cypher_for_item', 'cypher_for_node', 'cypher_for_edge_relation', 'NodeItem', 'EdgeRelationItem'
            'SchemaParser','Schema','NodeDefinition','EdgeDefinition']
-
-__version__ = '0.5'
---
+++ setup.cfg.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+VERSION=`python -c "import propgraph; print('.'.join(map(str,propgraph.__version__)))"`
+AUTHOR=`python -c "import propgraph; print(propgraph.__author__)"`
+EMAIL=`python -c "import propgraph; print(propgraph.__author_email__)"`
+DESCRIPTION=`python -c "import propgraph; print(propgraph.__doc__)"`
+REQUIRES=`python -c "list(map(print,['\t'+line.strip() for line in open('requirements.txt', 'r').readlines()]))"`
+cat <<EOF > setup.cfg
+[metadata]
+name = propgraph
+version = ${VERSION}
+author = ${AUTHOR}
+author_email = ${EMAIL}
+description = ${DESCRIPTION}
+license = MIT License
+url = https://github.com/alexmilowski/pypropgraph
+keywords=
+   cypher
+   property
+   graph
+   
+[options]
+packages =
+   propgraph
+include_package_data = True
+install_requires =
+${REQUIRES}
+
+[options.package_data]
+* = *.json, *.yaml, *.flow
+
+EOF
